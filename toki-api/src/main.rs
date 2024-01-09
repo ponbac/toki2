5d524c49b3a339ce442046f1bd96cb51d07441a2
ponbac <root@ponbac.xyz>
2024-01-09 17:57:27 +0100

    testing with two repos

--- toki-api/src/main.rs
+++ toki-api/src/main.rs
@@ -1,12 +1,18 @@
-use std::{env, sync::Arc};
+use std::{collections::HashMap, env, sync::Arc};
 
-use axum::{extract::State, response::IntoResponse, routing::get, Json, Router};
-use az_devops::RepoClient;
+use axum::{
+    extract::{Query, State},
+    http::StatusCode,
+    routing::get,
+    Json, Router,
+};
+use az_devops::{PullRequest, RepoClient};
+use serde::Deserialize;
 use tokio::net::TcpListener;
 
 #[derive(Clone)]
 struct AppState {
-    repo_client: Arc<RepoClient>,
+    repo_clients: Arc<HashMap<String, RepoClient>>,
 }
 
 #[tokio::main]
@@ -18,7 +24,15 @@ async fn main() {
     let repo_name = env::var("ADO_REPO").unwrap();
     let token = env::var("ADO_TOKEN").unwrap();
 
+    let organization_2 = env::var("ADO_ORGANIZATION_2").unwrap();
+    let project_2 = env::var("ADO_PROJECT_2").unwrap();
+    let repo_name_2 = env::var("ADO_REPO_2").unwrap();
+    let token_2 = env::var("ADO_TOKEN_2").unwrap();
+
     let repo_client = RepoClient::new(&repo_name, &organization, &project, &token)
+        .await
+        .unwrap();
+    let repo_client_2 = RepoClient::new(&repo_name_2, &organization_2, &project_2, &token_2)
         .await
         .unwrap();
 
@@ -26,7 +40,14 @@ async fn main() {
         .route("/", get(|| async { "Hello, World!" }))
         .route("/pull-requests", get(open_pull_requests))
         .with_state(AppState {
-            repo_client: Arc::new(repo_client),
+            repo_clients: Arc::new(
+                vec![
+                    (repo_name.to_lowercase(), repo_client),
+                    (repo_name_2.to_lowercase(), repo_client_2),
+                ]
+                .into_iter()
+                .collect(),
+            ),
         });
 
     println!("Listening on http://localhost:3000");
@@ -34,12 +55,34 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn open_pull_requests(State(app_state): State<AppState>) -> impl IntoResponse {
-    let pull_requests = app_state
-        .repo_client
-        .get_open_pull_requests()
-        .await
-        .unwrap();
+#[derive(Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct OpenPullRequestsQuery {
+    repo_name: String,
+}
 
-    Json(pull_requests)
+async fn open_pull_requests(
+    State(app_state): State<AppState>,
+    Query(query): Query<OpenPullRequestsQuery>,
+) -> Result<Json<Vec<PullRequest>>, (StatusCode, String)> {
+    let client = app_state
+        .repo_clients
+        .get(&query.repo_name.to_lowercase())
+        .ok_or((
+            StatusCode::NOT_FOUND,
+            format!(
+                "Repository '{}' not found. Available repositories: [{}]",
+                query.repo_name,
+                app_state
+                    .repo_clients
+                    .keys()
+                    .map(|s| s.to_string())
+                    .collect::<Vec<String>>()
+                    .join(", ")
+            ),
+        ))?;
+
+    let pull_requests = client.get_open_pull_requests().await.unwrap();
+
+    Ok(Json(pull_requests))
 }
