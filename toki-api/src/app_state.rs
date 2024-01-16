17520a401ff60a38d9f5b9f477a61440e08892ae
ponbac <root@ponbac.xyz>
2024-01-16 15:02:08 +0100

    use rw lock

--- toki-api/src/app_state.rs
+++ toki-api/src/app_state.rs
@@ -7,7 +7,7 @@ use axum::{
 use az_devops::RepoClient;
 use futures_util::{stream::FuturesUnordered, StreamExt};
 use sqlx::PgPool;
-use tokio::sync::Mutex;
+use tokio::sync::{Mutex, RwLock};
 
 use crate::domain::{RepoConfig, RepoKey};
 
@@ -30,7 +30,7 @@ impl IntoResponse for AppStateError {
 #[derive(Clone)]
 pub struct AppState {
     pub db_pool: Arc<PgPool>,
-    repo_clients: Arc<Mutex<HashMap<RepoKey, RepoClient>>>,
+    repo_clients: Arc<RwLock<HashMap<RepoKey, RepoClient>>>,
 }
 
 impl AppState {
@@ -61,7 +61,7 @@ impl AppState {
 
         Self {
             db_pool: Arc::new(db_pool),
-            repo_clients: Arc::new(Mutex::new(clients)),
+            repo_clients: Arc::new(RwLock::new(clients)),
         }
     }
 
@@ -69,7 +69,7 @@ impl AppState {
         &self,
         key: impl Into<RepoKey>,
     ) -> Result<RepoClient, AppStateError> {
-        let repo_clients = self.repo_clients.lock().await;
+        let repo_clients = self.repo_clients.read().await;
         let key: RepoKey = key.into();
 
         repo_clients
@@ -79,7 +79,7 @@ impl AppState {
     }
 
     pub async fn insert_repo_client(&self, key: impl Into<RepoKey>, client: RepoClient) {
-        let mut repo_clients = self.repo_clients.lock().await;
+        let mut repo_clients = self.repo_clients.write().await;
         let key: RepoKey = key.into();
 
         repo_clients.insert(key, client);
