9a1a9da9f58b13fe6e94768618a73c2c498c3d09
ponbac <root@ponbac.xyz>
2024-03-22 16:37:33 +0100

    hide devtools

--- app/src/routes/__root.tsx
+++ app/src/routes/__root.tsx
@@ -1,12 +1,24 @@
 import { RouterContext } from "@/main";
 import { Outlet, createRootRouteWithContext } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import React, { Suspense } from "react";
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === "production"
+    ? () => null // Render nothing in production
+    : React.lazy(() =>
+        // Lazy load in development
+        import("@tanstack/router-devtools").then((res) => ({
+          default: res.TanStackRouterDevtools,
+        })),
+      );
 
 export const Route = createRootRouteWithContext<RouterContext>()({
   component: () => (
     <>
       <Outlet />
-      <TanStackRouterDevtools />
+      <Suspense>
+        <TanStackRouterDevtools />
+      </Suspense>
     </>
   ),
 });
