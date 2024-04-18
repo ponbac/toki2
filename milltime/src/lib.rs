792fa0fc02bb9a979c4d22a213c1a0fcabee1788
ponbac <root@ponbac.xyz>
2024-04-19 00:06:08 +0200

    hmm, need tests

--- milltime/src/lib.rs
+++ milltime/src/lib.rs
@@ -8,3 +8,13 @@ pub(crate) use milltime_url::*;
 
 pub use auth::*;
 pub use client::*;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use chrono::{DateTime, Utc};
+    use std::env;
+
+    #[test]
+    fn test_create_credentials() {}
+}
