b45f7b644cbc210d2d4b899af83efa591d395b9e
ponbac <root@ponbac.xyz>
2024-04-19 08:58:01 +0200

    add a test

--- milltime/src/lib.rs
+++ milltime/src/lib.rs
@@ -12,9 +12,17 @@ pub use client::*;
 #[cfg(test)]
 mod tests {
     use super::*;
-    use chrono::{DateTime, Utc};
     use std::env;
 
-    #[test]
-    fn test_create_credentials() {}
+    #[tokio::test]
+    async fn test_create_credentials() {
+        dotenvy::from_filename("./milltime/.env.local").ok();
+        let username = env::var("MILLTIME_USERNAME").expect("MILLTIME_USERNAME must be set");
+        let password = env::var("MILLTIME_PASSWORD").expect("MILLTIME_PASSWORD must be set");
+
+        let credentials = Credentials::new(&username, &password).await.unwrap();
+        assert_eq!(credentials.username, Some(username));
+        assert_eq!(credentials.csrf_token.len(), 16);
+        assert!(credentials.valid_until.is_some());
+    }
 }
