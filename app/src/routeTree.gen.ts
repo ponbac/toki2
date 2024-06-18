4536a285b2a968938e7b42c7c0b234279416e315
Pontus Backman <root@ponbac.xyz>
2024-06-18 12:49:39 +0200

    upd

--- app/src/routeTree.gen.ts
+++ app/src/routeTree.gen.ts
@@ -87,46 +87,79 @@ const LayoutPrsPrIdRouteRoute = LayoutPrsPrIdRouteImport.update({
 declare module "@tanstack/react-router" {
   interface FileRoutesByPath {
     "/_layout": {
+      id: "/_layout"
+      path: ""
+      fullPath: ""
       preLoaderRoute: typeof LayoutRouteImport
       parentRoute: typeof rootRoute
     }
     "/login": {
+      id: "/login"
+      path: "/login"
+      fullPath: "/login"
       preLoaderRoute: typeof LoginImport
       parentRoute: typeof rootRoute
     }
     "/_layout/milltime": {
+      id: "/_layout/milltime"
+      path: "/milltime"
+      fullPath: "/milltime"
       preLoaderRoute: typeof LayoutMilltimeRouteImport
       parentRoute: typeof LayoutRouteImport
     }
     "/_layout/prs": {
+      id: "/_layout/prs"
+      path: "/prs"
+      fullPath: "/prs"
       preLoaderRoute: typeof LayoutPrsRouteImport
       parentRoute: typeof LayoutRouteImport
     }
     "/_layout/repositories": {
+      id: "/_layout/repositories"
+      path: "/repositories"
+      fullPath: "/repositories"
       preLoaderRoute: typeof LayoutRepositoriesRouteImport
       parentRoute: typeof LayoutRouteImport
     }
     "/_layout/": {
+      id: "/_layout/"
+      path: "/"
+      fullPath: "/"
       preLoaderRoute: typeof LayoutIndexImport
       parentRoute: typeof LayoutRouteImport
     }
     "/_layout/prs/$prId": {
+      id: "/_layout/prs/$prId"
+      path: "/$prId"
+      fullPath: "/prs/$prId"
       preLoaderRoute: typeof LayoutPrsPrIdRouteImport
       parentRoute: typeof LayoutPrsRouteImport
     }
     "/_layout/prs/commits": {
+      id: "/_layout/prs/commits"
+      path: "/commits"
+      fullPath: "/prs/commits"
       preLoaderRoute: typeof LayoutPrsCommitsRouteImport
       parentRoute: typeof LayoutPrsRouteImport
     }
     "/_layout/repositories/add": {
+      id: "/_layout/repositories/add"
+      path: "/add"
+      fullPath: "/repositories/add"
       preLoaderRoute: typeof LayoutRepositoriesAddRouteImport
       parentRoute: typeof LayoutRepositoriesRouteImport
     }
     "/_layout/prs/": {
+      id: "/_layout/prs/"
+      path: "/"
+      fullPath: "/prs/"
       preLoaderRoute: typeof LayoutPrsIndexImport
       parentRoute: typeof LayoutPrsRouteImport
     }
     "/_layout/repositories/": {
+      id: "/_layout/repositories/"
+      path: "/"
+      fullPath: "/repositories/"
       preLoaderRoute: typeof LayoutRepositoriesIndexImport
       parentRoute: typeof LayoutRepositoriesRouteImport
     }
@@ -135,21 +168,92 @@ declare module "@tanstack/react-router" {
 
 // Create and export the route tree
 
-export const routeTree = rootRoute.addChildren([
-  LayoutRouteRoute.addChildren([
+export const routeTree = rootRoute.addChildren({
+  LayoutRouteRoute: LayoutRouteRoute.addChildren({
     LayoutMilltimeRouteRoute,
-    LayoutPrsRouteRoute.addChildren([
+    LayoutPrsRouteRoute: LayoutPrsRouteRoute.addChildren({
       LayoutPrsPrIdRouteRoute,
       LayoutPrsCommitsRouteRoute,
       LayoutPrsIndexRoute,
-    ]),
-    LayoutRepositoriesRouteRoute.addChildren([
+    }),
+    LayoutRepositoriesRouteRoute: LayoutRepositoriesRouteRoute.addChildren({
       LayoutRepositoriesAddRouteRoute,
       LayoutRepositoriesIndexRoute,
-    ]),
+    }),
     LayoutIndexRoute,
-  ]),
+  }),
   LoginRoute,
-])
+})
 
 /* prettier-ignore-end */
+
+/* ROUTE_MANIFEST_START
+{
+  "routes": {
+    "__root__": {
+      "filePath": "__root.tsx",
+      "children": [
+        "/_layout",
+        "/login"
+      ]
+    },
+    "/_layout": {
+      "filePath": "_layout/route.tsx",
+      "children": [
+        "/_layout/milltime",
+        "/_layout/prs",
+        "/_layout/repositories",
+        "/_layout/"
+      ]
+    },
+    "/login": {
+      "filePath": "login.tsx"
+    },
+    "/_layout/milltime": {
+      "filePath": "_layout/milltime/route.tsx",
+      "parent": "/_layout"
+    },
+    "/_layout/prs": {
+      "filePath": "_layout/prs/route.tsx",
+      "parent": "/_layout",
+      "children": [
+        "/_layout/prs/$prId",
+        "/_layout/prs/commits",
+        "/_layout/prs/"
+      ]
+    },
+    "/_layout/repositories": {
+      "filePath": "_layout/repositories/route.tsx",
+      "parent": "/_layout",
+      "children": [
+        "/_layout/repositories/add",
+        "/_layout/repositories/"
+      ]
+    },
+    "/_layout/": {
+      "filePath": "_layout/index.tsx",
+      "parent": "/_layout"
+    },
+    "/_layout/prs/$prId": {
+      "filePath": "_layout/prs/$prId/route.tsx",
+      "parent": "/_layout/prs"
+    },
+    "/_layout/prs/commits": {
+      "filePath": "_layout/prs/commits/route.tsx",
+      "parent": "/_layout/prs"
+    },
+    "/_layout/repositories/add": {
+      "filePath": "_layout/repositories/add/route.tsx",
+      "parent": "/_layout/repositories"
+    },
+    "/_layout/prs/": {
+      "filePath": "_layout/prs/index.tsx",
+      "parent": "/_layout/prs"
+    },
+    "/_layout/repositories/": {
+      "filePath": "_layout/repositories/index.tsx",
+      "parent": "/_layout/repositories"
+    }
+  }
+}
+ROUTE_MANIFEST_END */
