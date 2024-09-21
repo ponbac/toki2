a7cffb14aee48a6092d0996b5a7c9d0b8d4f6604
ponbac <root@ponbac.xyz>
2024-09-21 12:23:39 +0200

    better

--- app/src/routes/_layout/milltime/-components/date-range-selector.tsx
+++ app/src/routes/_layout/milltime/-components/date-range-selector.tsx
@@ -1,6 +1,6 @@
 import { DateRange } from "react-day-picker";
-import { format, parseISO } from "date-fns";
-import { CalendarIcon } from "lucide-react";
+import { format, parseISO, startOfWeek, endOfWeek } from "date-fns";
+import { CalendarIcon, RotateCcw } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
   Popover,
@@ -9,6 +9,11 @@ import {
 } from "@/components/ui/popover";
 import { cn } from "@/lib/utils";
 import { Calendar } from "@/components/ui/calendar";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 export function DateRangeSelector(props: {
   dateRange: { from: string; to: string };
@@ -17,6 +22,10 @@ export function DateRangeSelector(props: {
   const dateRange = {
     from: parseISO(props.dateRange.from),
     to: parseISO(props.dateRange.to),
+  };
+  const thisWeekRange = {
+    from: startOfWeek(new Date(), { weekStartsOn: 1 }),
+    to: endOfWeek(new Date(), { weekStartsOn: 1 }),
   };
 
   const handleRangeSelect = (range: DateRange | undefined) => {
@@ -28,8 +37,14 @@ export function DateRangeSelector(props: {
     }
   };
 
+  const thisWeekSelected =
+    format(dateRange.from, "yyyy-MM-dd") ===
+      format(thisWeekRange.from, "yyyy-MM-dd") &&
+    format(dateRange.to, "yyyy-MM-dd") ===
+      format(thisWeekRange.to, "yyyy-MM-dd");
+
   return (
-    <div className="grid gap-2">
+    <div className="flex flex-row items-center gap-2">
       <Popover>
         <PopoverTrigger asChild>
           <Button
@@ -37,7 +52,7 @@ export function DateRangeSelector(props: {
             variant="outline"
             className={cn(
               "w-[300px] justify-start text-left font-normal",
-              !dateRange.from && "text-muted-foreground"
+              !dateRange.from && "text-muted-foreground",
             )}
           >
             <CalendarIcon className="mr-2 h-4 w-4" />
@@ -66,6 +81,24 @@ export function DateRangeSelector(props: {
           />
         </PopoverContent>
       </Popover>
+      {!thisWeekSelected && (
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button
+              onClick={() => handleRangeSelect(thisWeekRange)}
+              variant="ghost"
+              size="icon"
+              aria-label="Reset date range"
+              disabled={thisWeekSelected}
+            >
+              <RotateCcw className="h-4 w-4 disabled:text-red-500" />
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p className="text-sm">Reset date range to current week</p>
+          </TooltipContent>
+        </Tooltip>
+      )}
     </div>
   );
 }
