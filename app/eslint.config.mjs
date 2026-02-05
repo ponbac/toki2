de7a4d28eb76c8d05b634ced00afd2fc9601ccf9
ponbac <root@ponbac.xyz>
2026-02-05 12:18:00 +0100

    fix(lint): exclude components/ui from linting, restore max-warnings 0

--- app/eslint.config.mjs
+++ app/eslint.config.mjs
@@ -19,7 +19,7 @@ const compat = new FlatCompat({
 
 export default [
   {
-    ignores: ["**/dist", "**/.eslintrc.cjs"],
+    ignores: ["**/dist", "**/.eslintrc.cjs", "**/src/components/ui"],
   },
   ...pluginRouter.configs["flat/recommended"],
   ...fixupConfigRules(
